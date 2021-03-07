55e666429e5eb8905e2ea8b11cc8e0d13e7f98c5
Dulyfuly <garamibence@gmail.com>
2021-03-07 20:25:59 +0100

    Add basic support for #1
    Gives access for user service by a generated service id.

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,10 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
-	
+	@Test
+	public void CheckingServiceIdGenerationForTheNewFeature() {
+		Assert.assertEquals(0, user.getServiceIdForAllDevices());
+		user.generateServiceIdForAllDevices();
+		Assert.assertEquals(42, user.getServiceIdForAllDevices());
+	}
 }
